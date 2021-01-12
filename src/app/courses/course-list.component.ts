1e5aa34631225b202baff2a2c4f0143492a9304b
wslmacieiradev <wslmacieira@outlook.com>
2021-01-11 23:51:22 -0300

    cria StarComponent

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -9,26 +9,29 @@ export class CourseListComponent implements OnInit {
 
   courses: Course[] = [];
 
+  imagemLargura = 50;
+  imagemMargem = 2;
+
   ngOnInit(): void {
     this.courses = [
       {
         id: 1,
-        name: 'Angular',
-        imageUrl: '',
+        name: 'Angular Forms',
+        imageUrl: '../../assets/images/forms.png',
         price: 99.99,
         code: 'xps-763',
         duration: 120,
-        rating: 7.2,
+        rating: 4.5,
         releaseDate: '10/12/2020'
       },
       {
         id: 2,
-        name: '.Net',
-        imageUrl: '',
+        name: 'Angular Http',
+        imageUrl: '../../assets/images/http.png ',
         price: 99.99,
         code: 'hkc-763',
         duration: 90,
-        rating: 9.2,
+        rating: 4.2,
         releaseDate: '11/01/2021'
       }
     ];
