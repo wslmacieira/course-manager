d0d83c62911bd16bdfa4461c7d16b69fcb535521
wslmacieiradev <wslmacieira@outlook.com>
2021-01-12 00:08:09 -0300

    cria course.service

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from './course';
+import { CourseService } from './course.service';
 
 @Component({
   selector: 'app-course-list',
@@ -12,29 +13,12 @@ export class CourseListComponent implements OnInit {
   imagemLargura = 50;
   imagemMargem = 2;
 
+  constructor(private service: CourseService) {
+
+  }
+
   ngOnInit(): void {
-    this.courses = [
-      {
-        id: 1,
-        name: 'Angular Forms',
-        imageUrl: '../../assets/images/forms.png',
-        price: 99.99,
-        code: 'xps-763',
-        duration: 120,
-        rating: 4.5,
-        releaseDate: '10/12/2020'
-      },
-      {
-        id: 2,
-        name: 'Angular Http',
-        imageUrl: '../../assets/images/http.png ',
-        price: 99.99,
-        code: 'hkc-763',
-        duration: 90,
-        rating: 4.2,
-        releaseDate: '11/01/2021'
-      }
-    ];
+    this.courses = this.service.getAll();
   }
 
 }
